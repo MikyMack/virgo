2ab66547d3456544eb5ef8abcf2cf9f004525143
krishnaprasad <krishnaprasad8369@gmail.com>
2025-06-19 16:33:34 +0530

    navbar category added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@
 import './App.css'
 import React from 'react';
 import MainRoutes from './Routes/MainRoutes'
+import { Provider } from 'react-redux';
+import { store } from './components/Redux/Store';
 
 function App() {
   return (
    <React.StrictMode>
+    <Provider store={store}>
     <MainRoutes />
+    </Provider>
    </React.StrictMode>
   )
 }
